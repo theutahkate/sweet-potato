cbe1a1e2cdf668a7b3b175c72855fb9dc221902a
Utah Kate Newman <utah.kate.newman@gmail.com>
2018-09-16 17:01:31 -0700

    add search shows functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Initialize Firebase
 
-
 firebase.initializeApp(config);
 
 const database = firebase.database();
@@ -12,9 +11,7 @@ const addShowForm = document.getElementById("addShowForm"),
 			ul = document.querySelector(".show__list");
 let showTitle = "";
 
-console.log(ul);
-
-addShowForm.addEventListener("submit", function() {
+addShowForm.addEventListener("submit", () => {
 	event.preventDefault();
 	showTitle = titleInput.value;
 
@@ -26,8 +23,7 @@ addShowForm.addEventListener("submit", function() {
   addShowTitle = "";
 })
 
-database.ref("shows").on("value", function(results) {
-	console.log(results)
+database.ref("shows").on("value", (results) => {
 	ul.innerHTML = "";
 	let allShows = results.val();
 	for (var showID in allShows) {
@@ -39,7 +35,54 @@ database.ref("shows").on("value", function(results) {
 })
 
 
+// API
 
+let searchApi = query => {
+
+	let data = "{}";
+
+	const	baseImgUrl = "https://image.tmdb.org/t/p/w200_and_h300_bestv2",
+				baseUrl = "https://api.themoviedb.org/3/search/tv?api_key=",
+				middleUrl = "&language=en-US&query=",
+				endUrl = "&page=1";
+	let url = `${baseUrl}${tvApiKey}${middleUrl}${query}${endUrl}`;
+
+
+	let xhr = new XMLHttpRequest();
+	// xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+	  if (this.readyState === this.DONE) {
+	    data = JSON.parse(this.responseText).results;
+
+	    const	ul = document.querySelector('.search__list');
+
+			data.forEach(function(element) {
+				console.log(element)
+				const li = document.createElement('li');
+				let searchTitle = element.name,
+						searchImg = baseImgUrl + element.poster_path;
+	 			li.innerHTML = `<img src='${searchImg}'><h3>${searchTitle}</h3>`;
+	 			ul.appendChild(li);
+
+	 		})
+	  }
+	});
+
+	xhr.open("GET", url);
+
+	xhr.send(data);
+}
+
+searchForm.addEventListener("submit", () => {
+	event.preventDefault();
+	const searchForm = document.getElementById("searchForm"),
+				searchTitle = document.getElementById("searchTitle");
+	let searchQuery = searchTitle.value,
+			queryString = searchQuery.replace(/ /g, "%20");
+
+	searchApi(queryString);
+});
 
 
 
